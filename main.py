df71574223351f55f6d5d0c5e37fef0cefee53e6
Coleman <gibsonjc@rose-hulman.edu>
2016-10-12 23:21:12 +0000

    Added basic html skeleton, main serves content
    Far from even a complete skeleton but it has a little bit of structure.
    Added some obvious css (blue background) and js (alert) to show that it
    does, indeed, work.

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+#!/usr/bin/env python2
 
+import webapp2
+import os
+from jinja2 import Environment, FileSystemLoader
+
+# will likely need to replace this later
+template_path = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = Environment(loader=FileSystemLoader([template_path]))
+
+
+class BaseHandler(webapp2.RequestHandler):
+
+    def get(self):
+        template = self.get_template()
+        self.response.write(template.render())
+
+    def get_template(self):
+        return jinja_env.get_template('base-page.html')
+
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', BaseHandler)
+])
