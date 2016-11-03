3ca3167b80106fcd46cfae36a377d102a251929e
Coleman <gibsonjc@rose-hulman.edu>
2016-11-03 12:24:52 -0400

    Added a case for validation on the backend

--- handlers/dashboard_page_handler.py
+++ handlers/dashboard_page_handler.py
@@ -1,6 +1,7 @@
 
 from base_handler import BaseHandler
 import utils
+import re
 
 from google.appengine.api import users
 
@@ -8,6 +9,11 @@ from google.appengine.api import users
 class DashboardPageHandler(BaseHandler):
 
     template_name = 'dashboard-page/dashboard-page.html'
+    valid_name_re = re.compile(r'^\w+$')
+
+
+    def calendar_name_is_valid(self, name):
+        return DashboardPageHandler.valid_name_re.match(name) and True
 
     def get_template(self):
         return self.get_template_by_name(DashboardPageHandler.template_name)
@@ -22,6 +28,9 @@ class DashboardPageHandler(BaseHandler):
         email = auth_user.email().lower()
 
         calendar_name = self.request.get('calendar-name')
-        calendar = utils.create_default_calendar(user, calendar_name)
+        if not self.calendar_name_is_valid(calendar_name):
+            # TODO deal with this
+            return False
 
+        calendar = utils.create_default_calendar(user, calendar_name)
         self.redirect('/calendar/%s/%s' % (user.username, calendar_name))
