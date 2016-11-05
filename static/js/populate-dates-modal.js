3bb56a844c6eb307ebd1c8841442e9969c19f885
Coleman <gibsonjc@rose-hulman.edu>
2016-11-05 17:01:05 -0400

    created basic bootstrap modal interface

---
+++ static/js/populate-dates-modal.js
@@ -0,0 +1,9 @@
+
+(function(cns, cpn, cetn) {
+
+    cetn.populate_dates_modal_namespace = cetn.populate_dates_modal_namespace || {};
+    let pdmn = cetn.populate_dates_modal_namespace;
+
+})(calendar_namespace,
+   calendar_namespace.calendar_page_namespace,
+   calendar_namespace.calendar_page_namespace.calendar_edit_toolbar_namespace);
