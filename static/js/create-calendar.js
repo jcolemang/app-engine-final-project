97a3f970f2d377cae1feb97e1a6a3294f9d68a14
Coleman <gibsonjc@rose-hulman.edu>
2016-11-06 01:23:41 -0400

    fixed weird reordering problem in a file

--- static/js/create-calendar.js
+++ static/js/create-calendar.js
@@ -2,13 +2,14 @@
 
 (function(cns) {
 
-    ccns.CreateCalendarValidator.prototype.validateName = function(name) {
-        cns.create_calendar_namespace = cns.create_calendar_namespace || {};
-        let ccns = cns.create_calendar_namespace;
+    cns.create_calendar_namespace = cns.create_calendar_namespace || {};
+    let ccns = cns.create_calendar_namespace;
 
-        ccns.CreateCalendarValidator = function() {
-            this.check_name_re = /^\w+$/;
-        };
+    ccns.CreateCalendarValidator = function() {
+        this.check_name_re = /^\w+$/;
+    };
+
+    ccns.CreateCalendarValidator.prototype.validateName = function(name) {
         return this.check_name_re.test(name);
     };
 
